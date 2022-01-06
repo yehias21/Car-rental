6dcda5c97d74d3990c4e3ff5d013ef5975e14699
omar <omarmohameddrive@gmail.com>
2022-01-06 18:05:51 +0200

    working on system reports - json

--- carSQL.py
+++ carSQL.py
@@ -6,7 +6,7 @@ DB_NAME = "carsystem"
 DB_USER = "postgres"
 DB_PASS = "postgres"
 conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASS)
-db = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+db = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
 
 def login_query(tablename=None):
@@ -24,6 +24,7 @@ car_search = "SELECT car.* , car_image.image FROM car natural join car_image WHE
 
 car_search_plate = "SELECT car.* , car_image.image FROM car natural join car_image WHERE plateid = %s "
 update_car = "UPDATE car SET (active,rate) = (%s,%s) WHERE plateid = %s"
+search_customer = "select * from customer where username = %s"
 
 # reports:
 all_reservations = "SELECT reserve_date,pickup_date,return_date,concat(fname,lname) customer_name,plateid,bill,paid " \
@@ -34,6 +35,10 @@ all_reservations = "SELECT reserve_date,pickup_date,return_date,concat(fname,lna
 car_reservations_date = "select reserve_date,pickup_date,return_date,bill,paid from reservation " \
                         "where carid = %s and (reserve_date between %s and %s)"
 
-customer_reservations_date = "select reserve_date ,pickup_date , return_date , bill ,paid , carid , concat(brand,model) car_model " \
+customer_reservations = "select reserve_date ,pickup_date , return_date , bill ,paid , carid , concat(brand,model) car_model " \
                              "from reservation join car c on c.plateid = reservation.carid where custid = %s"
+
+customer_payments = "select carid,reserve_date,amount,date " \
+                    "from (payments join customer c on c.id = payments.custid) as cp " \
+                    "join reservation r on cp.rid = r.rid where custid = %s"
 
