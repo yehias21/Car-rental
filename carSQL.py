7c6ba18c9206397d2a16ad744099c14ebc2cf69a
omar <omarmohameddrive@gmail.com>
2022-01-06 21:06:25 +0200

    added car image to json request

--- carSQL.py
+++ carSQL.py
@@ -15,30 +15,36 @@ def login_query(tablename=None):
     return sql.SQL("SELECT * FROM {table} WHERE username = %s").format(table=sql.Identifier(tablename))
 
 
-customer_register = "INSERT INTO customer (username, password, fname, lname, email, country, city, address) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+customer_register = "INSERT INTO customer (username, password, fname, lname, email, country, city, address) VALUES (" \
+                    "%s,%s,%s,%s,%s,%s,%s,%s) "
 admin_register = "INSERT INTO admin (username, password) VALUES (%s,%s)"
 car_register = "INSERT INTO car (plateid,modelyr,brand,model,color,rate) VALUES (%s,%s,%s,%s,%s,%s)"
 car_image_register = "INSERT INTO car_image (plateid, image) values (%s,%s)"
-car_search = "SELECT car.* , car_image.image FROM car natural join car_image WHERE " \
+car_search = "SELECT car.plateid, extract(year from car.modelyr) as year, brand, model, color, active, rate , " \
+             "encode(car_image.image,'hex') img " \
+             "FROM car natural join car_image WHERE " \
              "plateid = %s or modelyr = %s or brand = %s or model=%s or color=%s or active = %s or rate = %s"
 
-car_search_plate = "SELECT car.* , car_image.image FROM car natural join car_image WHERE plateid = %s "
+car_search_plate = "SELECT car.plateid, extract(year from car.modelyr) as year, brand, model, color, active, rate , " \
+                   "encode(car_image.image,'hex') img " \
+                   "FROM car natural join car_image WHERE plateid = %s "
+
 update_car = "UPDATE car SET (active,rate) = (%s,%s) WHERE plateid = %s"
 search_customer = "select * from customer where username = %s"
 
 # reports:
 all_reservations = "SELECT reserve_date,pickup_date,return_date,concat(fname,lname) customer_name,plateid,bill,paid " \
-               "FROM (reservation join customer c on c.id = reservation.custid) as rc join car on rc.carid = " \
-               "car.plateid " \
-               "where reserve_date between %s and %s "
+                   "FROM (reservation join customer c on c.id = reservation.custid) as rc join car on rc.carid = " \
+                   "car.plateid " \
+                   "where reserve_date between %s and %s "
 
 car_reservations_date = "select reserve_date,pickup_date,return_date,bill,paid from reservation " \
                         "where carid = %s and (reserve_date between %s and %s)"
 
-customer_reservations = "select reserve_date ,pickup_date , return_date , bill ,paid , carid , concat(brand,model) car_model " \
-                             "from reservation join car c on c.plateid = reservation.carid where custid = %s"
+customer_reservations = "select reserve_date ,pickup_date , return_date , bill ,paid , carid , concat(brand," \
+                        "model) car_model " \
+                        "from reservation join car c on c.plateid = reservation.carid where custid = %s"
 
 customer_payments = "select carid,reserve_date,amount,date " \
                     "from (payments join customer c on c.id = payments.custid) as cp " \
                     "join reservation r on cp.rid = r.rid where custid = %s"
-
