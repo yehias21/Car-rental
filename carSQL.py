ab67e786a0f6cdf358402959733653c2b185b0cc
AhmedOsamaElShafei <AhmedOsamaElShafei>
2022-01-07 04:56:04 +0200

    Modified customer search, customer entries.

--- carSQL.py
+++ carSQL.py
@@ -1,3 +1,5 @@
+import datetime
+
 from psycopg2 import sql
 import psycopg2.extras
 
@@ -7,7 +9,7 @@ DB_USER = "postgres"
 DB_PASS = "postgres"
 conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASS)
 db = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
+default_date = datetime.date(1970,1,1)
 
 def login_query(tablename=None):
     if tablename is None:
@@ -21,6 +23,8 @@ car_register = "INSERT INTO car (plateid,modelyr,brand,model,color,rate) VALUES 
 car_image_register = "INSERT INTO car_image (plateid, image) values (%s,%s)"
 car_search = "SELECT car.* , car_image.image FROM car natural join car_image WHERE " \
              "plateid = %s or modelyr = %s or brand = %s or model=%s or color=%s or active = %s or rate = %s"
+car_search_customer = "SELECT car.* , car_image.image FROM car natural join car_image WHERE " \
+             "plateid = %s or modelyr = %s or brand = %s or model=%s or color=%s or rate = %s and active = %s"
 
 car_search_plate = "SELECT car.* , car_image.image FROM car natural join car_image WHERE plateid = %s "
 update_car = "UPDATE car SET (active,rate) = (%s,%s) WHERE plateid = %s"
--- customer.py
+++ customer.py
@@ -1,7 +1,9 @@
 from flask import (Blueprint, flash, redirect, render_template, request, url_for, session)
 from auth import login_required
+import datetime
 
 import carSQL as sql
+import car as car
 
 bp = Blueprint('customer', __name__, url_prefix='/customer')
 conn = sql.conn
@@ -11,6 +13,40 @@ db = sql.db
 @bp.route('/customer_home', methods=["GET", "POST"])
 def home():
     return render_template("customer/customer_home.html")
+
+
+@bp.route('/customer_search', methods=["GET", "POST"])
+def search_car():
+    if request.method == "POST":
+    #     content = request.form
+    #     brand = content["brand"]
+    #     model = content["model"]
+    #     model_year = content["model_year"]
+    #     model_year = model_year if model_year != '' else '1970'
+    #     model_year = datetime.date(int(model_year), 1, 1)
+    #     color = content["color"]
+    #     plateID = ''
+    #     active = True
+    #     rate = ''
+    #     db.execute(sql.car_search_customer, (plateID, model_year, brand, model, color, rate, active))
+    #     cars = db.fetchall()
+    #     return render_template("cars.html", cars=cars)
+        content = request.json
+        where = " WHERE "
+        if (request.form['brand']): where += f" brand={request.form['brand']} and"
+        if (request.form['model']): where += f" model={request.form['model']} and"
+        if (request.form['color']): where += f" color={request.form['color']} and"
+        if (request.form['start_date']): where += f"pickup_date not between " \
+                                             f"'{request.form['start_date']}' and '{request.form['end_date']}' and '{request.form['start_date']}' not between " \
+                                             f"pickup_date and return_date and"
+        query = "SELECT  brand,model,rate,extract(year from car.modelyr),encode(car_image.image,'hex') img " \
+                "FROM car natural join car_image left join reservation on car.plateid=reservation.carid" \
+                + where + " active=true;"
+        print(query)
+        db.execute(query)
+        cars = db.fetchall()
+        return render_template("cars.html", cars=cars)
+    return render_template("customer/customer_search_car.html")
 
 
 @bp.route('/logout')
