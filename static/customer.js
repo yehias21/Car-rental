6f8a92fc13c5f84ec96173d4ab146c8a3c2fb390
Ibrahim ELGhotmi <elghotmy7@gmail.com>
2022-01-09 06:52:33 +0200

    Customer search page edits and added overlay floating menu

--- static/customer.js
+++ static/customer.js
@@ -2,3 +2,23 @@ async function searchCars(Brand,Model){
     console.log(Brand+Model);
     prevent
 }
+
+// Modal
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+//
